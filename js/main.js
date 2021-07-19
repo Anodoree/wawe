0cf0b85539864f39c3f1ad7b7fe8ada05ae692be
Anodoree <emfwws@gmail.com>
2021-07-19 14:25:29 +0300

    add smooth scroll

--- js/main.js
+++ js/main.js
@@ -12,6 +12,21 @@ $(function() {
     $('.nav__list').removeClass('nav__list--active');
   });
 
+  $(".nav__link").on("click", smoothScroll);
+  $(".logo").on("click", smoothScroll);
+  $(".header__icon").on("click", smoothScroll);
+
+  function smoothScroll(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  };
+
+
+
   $('.blog__slider').slick({
     autoplay: true,
     arrows: false,
