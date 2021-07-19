0e48f39da0d21dd9ecc0f6bcbcc72407a311becc
Anodoree <emfwws@gmail.com>
2021-07-19 03:20:09 +0300

    adapted first 4 sections, pre-alpha

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-$(function(){
-  var mixer = mixitup('.gallery__list');
+$(function() {
+
+  $('.nav__btn').on('click', function() {
+    $('.nav__list').toggleClass('nav__list--active');
+  });
+
+  $('.nav__link').on('click', function() {
+    $('.nav__list').removeClass('nav__list--active');
+  });
+
+  $('.header__logo').on('click', function() {
+    $('.nav__list').removeClass('nav__list--active');
+  });
 
   $('.blog__slider').slick({
     autoplay: true,
     arrows: false,
     dots: true,
   });
+
+  var mixer = mixitup('.gallery__list');
+
 });
