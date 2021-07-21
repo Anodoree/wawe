b49a4f31ffea94e94d0cbb4838d932fd77a63980
Anodoree <emfwws@gmail.com>
2021-07-22 00:54:31 +0300

    adapted all - alpha, corrections 4

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(function() {
 
-  $('.nav__btn').on('click', function() {
+  $('.header__burger').on('click', function() {
     $('.header__menu').toggleClass('header__menu--active');
+    $('.burger__lay').toggleClass('burger__lay--active');
+    $('body').toggleClass('overflow-hidden');
   });
 
   $('.menu__link').on('click', function() {
     $('.header__menu').removeClass('header__menu--active');
-  });
-
-  $('.header__logo').on('click', function() {
-    $('.header__menu').removeClass('header__menu--active');
+    $('.burger__lay').toggleClass('burger__lay--active');
+    $('body').toggleClass('overflow-hidden');
   });
 
   $(".menu__link").on("click", smoothScroll);
@@ -28,7 +28,6 @@ $(function() {
 
 
   $('.blog__list').slick({
-    autoplay: true,
     arrows: false,
     dots: true,
   });
