718d18e1f3594f3e43ebbdec8c945fb46f6aa865
Anodoree <emfwws@gmail.com>
2021-07-22 14:33:39 +0300

    add sticky header - alpha

--- js/main.js
+++ js/main.js
@@ -6,24 +6,31 @@ $(function() {
     $('body').toggleClass('overflow-hidden');
   });
 
-  $('.menu__link').on('click', function() {
+  $('.header__logo, .menu__link').on('click', function() {
     $('.header__menu').removeClass('header__menu--active');
-    $('.burger__lay').toggleClass('burger__lay--active');
-    $('body').toggleClass('overflow-hidden');
+    $('.burger__lay').removeClass('burger__lay--active');
+    $('body').removeClass('overflow-hidden');
   });
 
-  $(".menu__link").on("click", smoothScroll);
-  $(".logo").on("click", smoothScroll); 
-  $(".header__icon").on("click", smoothScroll);
-
-  function smoothScroll(event) {
+  $(".menu__link, .logo, .header__icon").on("click", function(event) {
     event.preventDefault();
     var id = $(this).attr('href'),
       top = $(id).offset().top;
     $('body,html').animate({
       scrollTop: top
     }, 1500);
-  };
+  });
+
+  $(window).scroll(function() {
+    let i = document.documentElement.clientHeight;
+    if ($(this).scrollTop() >= i) {
+      $('.header__nav').addClass('header__nav--fixed');
+      $('.header__logo').addClass('header__logo--fixed');
+    } else {
+      $('.header__nav').removeClass('header__nav--fixed');
+      $('.header__logo').removeClass('header__logo--fixed');
+    }
+  });
 
 
 
